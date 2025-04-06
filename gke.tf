7178f05103e0b486a44d49aa4eaeeba02c1a1579
hynux-admin <raghulm@hynux.tech>
2025-04-06 22:14:01 +0530

    Update gke.tf

--- gke.tf
+++ gke.tf
@@ -1,23 +1,14 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-  }
-}
-
 provider "google" {
-  project = "custom-altar-455808-t3"
+  project = "custom-altar-455808-t3"  # Replace with your GCP Project ID
   region  = "us-central1"
 }
 
 resource "google_container_cluster" "gke_standard" {
-  name     = "gke-standard-cluster"
+  name     = "my-gke-cluster"
   location = "us-central1"
 
-  enable_autopilot    = false
-  initial_node_count  = 1  # ✅ Fix: GKE requires at least 1 initial node
+  remove_default_node_pool = true
+  initial_node_count       = 1  # Required but won't create default nodes
 
   networking_mode = "VPC_NATIVE"
   ip_allocation_policy {}
@@ -30,19 +21,9 @@ resource "google_container_node_pool" "primary_nodes" {
   node_count = 1  
 
   node_config {
-    machine_type = "e2-small"
-    disk_size_gb = 20
-    disk_type    = "pd-standard"  # ✅ Uses HDD instead of SSD
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
-    metadata = {
-      disable-legacy-endpoints = "true"
-    }
-  }
-
-  management {
-    auto_repair  = true
-    auto_upgrade = true
+    machine_type = "e2-small"  # Small instance to fit quotas
+    disk_size_gb = 20          # ✅ Reduced to fit within your 250 GB quota
+    disk_type    = "pd-ssd"
+    oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 }
